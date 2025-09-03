30b7c91fbe02a800af69fffb274951f79cbb1470
Guillermo Rodriguez <guilledk@protonmail.com>
2025-09-03 17:51:45 -0300

    Make Transform into custom class and generalize

--- src/native_db/transform.py
+++ src/native_db/transform.py
@@ -1,11 +1,12 @@
-import msgspec
+from pathlib import Path
+from typing import Any
 
 import polars as pl
 
-from native_db.table import Table
+from native_db._utils import path_size
 
 
-class Transform(msgspec.Struct, frozen=True):
+class Transform:
     '''
     Declare a dataframe transform using a pl.LazyFrame, and a cache path and
     provide a cached scan_parquet api to the materialized dataframe.
@@ -14,30 +15,46 @@ class Transform(msgspec.Struct, frozen=True):
     data.
 
     '''
+    def __init__(
+        self,
+        name: str,
+        query: pl.LazyFrame,
+        cache_path: Path,
+        sink_args: dict[str, Any] = {}
+    ) -> None:
+        self.name = name
+        self.query = query
+        self.cache_path = cache_path
+        self.sink_args = sink_args
 
-    query: pl.LazyFrame
-    meta: Table
+        self._frame: pl.LazyFrame | None = None
 
     @property
     def is_cached(self) -> bool:
-        return self.meta.exists
+        return self.cache_path.exists()
 
     @property
     def disk_size(self) -> int:
-        return self.meta.disk_size
+        return path_size(self.cache_path)
 
-    def scan(self) -> pl.LazyFrame:
+    def scan(self, use_cache: bool = True) -> pl.LazyFrame:
         '''
         Materialize transform to meta.local_path if not present already, then
         return a `pl.LazyFrame` to it.
 
         '''
+        if use_cache and self._frame is not None:
+            return self._frame
+
         if not self.is_cached:
             self.query.sink_parquet(
-                self.meta.local_path,
-                compression=self.meta.compression,
-                row_group_size=self.meta.schema.row_group_size,
+                self.cache_path,
                 mkdir=True,
+                **self.sink_args
             )
 
-        return pl.scan_parquet(self.meta.local_path)
+        frame = pl.scan_parquet(self.cache_path)
+        if use_cache:
+            self._frame = frame
+
+        return frame
